c62141a00e4a7e60f82d2777328a57b71f2af46c
NARMADHA-P <narmadha501@gmail.com>
2018-05-07 16:12:13 +0530

    Create vowelsorconstants.c

---
+++ vowelsorconstants.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+char a;
+if ((a>=97&&a<=122)||(a>=65&&a<=90))
+{
+  if(a=='a'||a=='A"||a=='e'||a=='E'||a=='i'||a=='I"||a=='o'||a=='O'||a=='u'||a=='U')
+     printf("Vowels");
+  else printf("Consonant");
+}
+else printf("Invalid input");
+}
