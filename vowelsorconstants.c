7e71ecfc8f224f701e796639f3c32f81dbd9e4a6
NARMADHA-P <narmadha501@gmail.com>
2018-05-07 16:12:57 +0530

    Update vowelsorconstants.c

--- vowelsorconstants.c
+++ vowelsorconstants.c
@@ -5,7 +5,7 @@ char a;
 if ((a>=97&&a<=122)||(a>=65&&a<=90))
 {
   if(a=='a'||a=='A"||a=='e'||a=='E'||a=='i'||a=='I"||a=='o'||a=='O'||a=='u'||a=='U')
-     printf("Vowels");
+     printf("Vowel");
   else printf("Consonant");
 }
 else printf("Invalid input");
